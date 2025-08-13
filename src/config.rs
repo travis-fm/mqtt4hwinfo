37d05883e6114183b68138e309c8fd6f93dce804
travis-fm <1546593+travis-fm@users.noreply.github.com>
2025-08-13 16:19:21 -0400

    Add sensor_key_name for sensors

--- src/config.rs
+++ src/config.rs
@@ -17,11 +17,12 @@ pub struct DeviceConfig {
 #[derive(Deserialize)]
 pub struct SensorConfig {
     pub sensor_name: String,
+    pub sensor_key_name: String,
     pub sensor_type: SensorType,
     pub mqtt_topic: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, PartialEq, PartialOrd)]
 pub enum SensorType {
     Temperature,
     Voltage,
@@ -33,6 +34,24 @@ pub enum SensorType {
     Other,
 }
 
+impl SensorType {
+    /// Converts SensorType value into string used for the key base name in the registry.
+    ///
+    /// See https://www.hwinfo.com/forum/threads/custom-user-sensors-in-hwinfo.5817/
+    fn to_key_base_string(&self) -> String {
+        match self {
+            SensorType::Temperature => String::from("Temp"),
+            SensorType::Voltage => String::from("Volt"),
+            SensorType::Fan => String::from("Fan"),
+            SensorType::Current => String::from("Current"),
+            SensorType::Power => String::from("Power"),
+            SensorType::Clock => String::from("Clock"),
+            SensorType::Usage => String::from("Usage"),
+            SensorType::Other => String::from("Other"),
+        }
+    }
+}
+
 #[derive(Deserialize)]
 pub struct Config {
     pub broker: BrokerConfig,
@@ -41,8 +60,15 @@ pub struct Config {
 
 impl Config {
     pub fn load() -> Self {
-        let config_str: Config =
+        let mut config_str: Config =
             toml::from_str(include_str!("../config.toml")).expect("Failed to parse config.toml");
+
+        for device in &mut config_str.devices {
+            for (i, sensor) in device.sensors.iter_mut().enumerate() {
+                sensor.sensor_key_name =
+                    sensor.sensor_type.to_key_base_string() + i.to_string().as_str();
+            }
+        }
 
         config_str
     }
